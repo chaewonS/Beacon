f7c9dfce363de9b56425eea746c8ca3acbda7e14
alalai012 <65814554+alalai012@users.noreply.github.com>
2022-01-22 20:48:41 +0900

    [선주] 좌표json파일 생성 코드
    비콘 MAC주소를이름으로 좌표와 장소이름을 추가하면 test.json 파일이 생성됩니다.

---
+++ make_json.py
@@ -0,0 +1,30 @@
+import json
+
+Beacon = dict()
+
+beacon1 = dict()
+
+beacon1["location"] = "0,0"
+beacon1["place"] = "좌상단"
+Beacon["00:19:01:70:81:ED"] = beacon1
+
+beacon2 = dict()
+beacon2["location"] = "8,0"
+beacon2["place"] = "우상단"
+Beacon["00:19:01:70:85:95"] = beacon2
+
+beacon3 = dict()
+beacon3["location"] = "0,14"
+beacon3["place"] = "좌하단"
+Beacon["00:19:01:70:85:C3"] = beacon3
+
+beacon4 = dict()
+beacon4["location"] = "8,14"
+beacon4["place"] = "우하단"
+Beacon["00:19:01:70:84:4B"] = beacon4
+
+
+with open('./test.json', 'w', encoding='utf-8')as make_file:
+    json.dump(Beacon, make_file, indent='\t')
+
+print("위치 좌표 파일이 현재 위치에 'test.json'으로 갱신되었습니다.")
