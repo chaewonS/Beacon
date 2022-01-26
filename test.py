22e0f781f4c2788321947979ae66c277b12a81f3
alalai012 <tjswn0125@naver.com>
2022-01-26 09:15:22 +0900

    특정 비콘의 신호만의 거리 출력

--- test.py
+++ test.py
@@ -8,6 +8,25 @@ import sys
 
 import bluetooth._bluetooth as bluez
 
+#FB301BC 초기 설정 TxPower = 41
+def calculateDistance(rssi) :
+    txPower = 41
+    if (rssi == 0) :
+        return -1.0 #if we cannot determine distance return -1.
+    ratio = rssi*1.0/txPower #
+    if (ratio < 1.0) :
+        return ratio**10
+  
+    else:
+        accuracy =  (0.89976)*(ratio**7.7095) + 0.111
+        return accuracy
+# #https://github.com/location-competition/indoor-location-competition-20
+
+def simpleDistance(rssi):
+    TxPower = 41
+    return 10 ** ((TxPower - rssi )/(10/4)) # 4 = n : 실내공간
+
+
 dev_id = 0
 try:
         sock = bluez.hci_open_dev(dev_id)
@@ -25,5 +44,8 @@ while True:
         print "----------"
         for beacon in returnedList:
                 if beacon[:17] == '00:19:01:70:81:': #특정 비콘을 선택해서 출력
+                        rssi = beacon[66:]
                         print beacon
-
+                        print "rssi :  %s" %rssi
+                        print "distance of calc : %f" %calculateDistance(int(rssi))
+                        print "distance of simple : %f" %simpleDistance(int(rssi))
