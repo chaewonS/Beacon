128185c0634dba4cab0db4d52f74da2980718f11
alalai012 <65814554+alalai012@users.noreply.github.com>
2022-03-06 18:37:20 +0900

    [선주] invalid keyword argument 오류 수정
    Traceback (most recent call last):
      File "make_json.py", line 28, in <module>
        with open('./test.json', 'w', encoding='utf-8')as make_file:
    TypeError: 'encoding' is an invalid keyword argument for this function 오류 수정

--- make_json.py
+++ make_json.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import io
 import json
 
 Beacon = dict()
@@ -12,7 +13,7 @@ Beacon["00:19:01:70:81:ed"] = beacon1
 beacon2 = dict()
 beacon2["location"] = "8,0"
 beacon2["place"] = "우상단"
-Beacon["00:19:01:70:85:95"] = beacon2
+Beacon["00:19:01:70:82:62"] = beacon2
 
 beacon3 = dict()
 beacon3["location"] = "0,14"
@@ -22,10 +23,11 @@ Beacon["00:19:01:70:85:c3"] = beacon3
 beacon4 = dict()
 beacon4["location"] = "8,14"
 beacon4["place"] = "우하단"
-Beacon["00:19:01:70:84:4b"] = beacon4
+Beacon["00:19:01:70:82:cb"] = beacon4
 
 
-with open('./test.json', 'w', encoding='utf-8')as make_file:
+with io.open('./test.json', 'wb',
+# encoding='utf-8'
+)as make_file:
     json.dump(Beacon, make_file)
-
 print("위치 좌표 파일이 현재 위치에 'test.json'으로 갱신되었습니다.")
