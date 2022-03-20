d442989f0dd0916bbfc0cdc5c7f7b7949e290ae4
alalai012 <65814554+alalai012@users.noreply.github.com>
2022-03-20 20:07:27 +0900

    [선주] 진행사항을 알 수 있도록 비콘정보 및 작성되는 데이터 수 출력

--- make_csv.py
+++ make_csv.py
@@ -32,6 +32,7 @@ with open('distance_RSSI.csv', 'wb') as f:
                 #if beacon[:17] == '00:19:01:70:81:': #특정 비콘을 선택해서 출력
                 if beacon[:5] =='00:19':
                         print beacon
+                        print distance + "거리에서 " + maden +"개의 신호가 작성되는 중..."
                         rssi = beacon[66:]
                         data = (str(distance)+" "+ str(rssi)).split(" ")
                         if(data.__len__() is 2):#오류 없이 됐을 때만 반복 체크 및 해당 데이터 작성
