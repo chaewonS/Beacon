89e7865f1ef3ddeec2c839852197708223ac5395
Sunjoo Park <tjswn0125@naver.com>
2022-03-20 20:03:12 +0900

    [선주] python2 버전으로 파일 입력 수정

--- make_csv.py
+++ make_csv.py
@@ -8,36 +8,35 @@ import csv
 
 
 distance = 0.1# 측정 시작 거리
+with open('distance_RSSI.csv', 'wb') as f:
+    w=csv.writer(f)
 
-f = open('distance_RSSI.csv', "w", newline='', encoding="utf-8-sig")
-w=csv.writer(f)
+    dev_id = 0
+    try:
+            sock = bluez.hci_open_dev(dev_id)
+            print "ble thread started"
 
-dev_id = 0
-try:
-        sock = bluez.hci_open_dev(dev_id)
-        print "ble thread started"
+    except:
+            print "error accessing bluetooth device..."
+            sys.exit(1)
 
-except:
-        print "error accessing bluetooth device..."
-        sys.exit(1)
+    blescan.hci_le_set_scan_parameters(sock)
+    blescan.hci_enable_le_scan(sock)
 
-blescan.hci_le_set_scan_parameters(sock)
-blescan.hci_enable_le_scan(sock)
+    for i in range(30):
+        maden = 0 #각 반복마다 작성된 신호의 수
+        while maden <5000:
+            returnedList = blescan.parse_events(sock, 10)
+            print "----------"
+            for beacon in returnedList:
+                #if beacon[:17] == '00:19:01:70:81:': #특정 비콘을 선택해서 출력
+                if beacon[:5] =='00:19':
+                        rssi = beacon[66:]
+                        data = (str(distance)+" "+ str(rssi)).split(" ")
+                        if(data.__len__() is 2):#오류 없이 됐을 때만 반복 체크 및 해당 데이터 작성
+                            maden += 1
+                            w.writerow(data)
 
-for i in range(30):
-    maden = 0 #각 반복마다 작성된 신호의 수
-    while maden <5000:
-        returnedList = blescan.parse_events(sock, 10)
-        print "----------"
-        for beacon in returnedList:
-            #if beacon[:17] == '00:19:01:70:81:': #특정 비콘을 선택해서 출력
-            if beacon[:5] =='00:19':
-                    rssi = beacon[66:]
-                    data = (str(distance)+" "+ str(rssi)).split(" ")
-                    if(data.__len__() is 2):#오류 없이 됐을 때만 반복 체크 및 해당 데이터 작성
-                        maden += 1
-                        w.writerow(data)
-
-    input("비콘의 거리를 10cm 멀리 조정해주세요. <완료시 Enter>")
-    distance += 0.1
-    print("거리"+ str(distance) +"(으)로 측정을 시작합니다.")
+        input("비콘의 거리를 10cm 멀리 조정해주세요. <완료시 Enter>")
+        distance += 0.1
+        print("거리"+ str(distance) +"(으)로 측정을 시작합니다.")
