970e1ca50a58ac9926bd6a8076c5883f39b07023
alalai012 <tjswn0125@naver.com>
2022-04-01 17:35:32 +0900

    [선주] 좌표 넓이 상수에서 변수로 적용

--- sunjoo/GUI.py
+++ sunjoo/GUI.py
@@ -153,25 +153,21 @@ if __name__ == '__main__':
     canvas = Tkinter.Canvas(master, width=width, height=height)
 
     rectanglesize = 7
+    rows = range(1,rectanglesize)
+    columns = range(1, rectanglesize)
     SP_w = 215 #1080-650/2
     SP_h = 10 #StartPoint
     polygon = canvas.create_rectangle(SP_w,SP_h, width-SP_w,height-SP_h)
 
     row = int((height-SP_h -SP_h)/rectanglesize) # 행 간격
-    x1 = canvas.create_line(SP_w, SP_h+row*1, width-SP_w, SP_h+row*1, fill="red")
-    x2 = canvas.create_line(SP_w, SP_h+row*2, width-SP_w, SP_h+row*2)
-    x3 = canvas.create_line(SP_w, SP_h+row*3, width-SP_w, SP_h+row*3)
-    x4 = canvas.create_line(SP_w, SP_h+row*4, width-SP_w, SP_h+row*4)
-    x5 = canvas.create_line(SP_w, SP_h+row*5, width-SP_w, SP_h+row*5)
-    x6 = canvas.create_line(SP_w, SP_h+row*6, width-SP_w, SP_h+row*6)
+
+    for i in rows:
+        i = canvas.create_line(SP_w, SP_h+row*i, width-SP_w, SP_h+row*i)
+
 
     column = int((width-SP_w-SP_w)/rectanglesize) #열 간격
-    y1 = canvas.create_line(SP_w+column, SP_h, SP_w +column, height-SP_h, fill="red")
-    y2 = canvas.create_line(SP_w+column*2, SP_h, SP_w +column*2, height-SP_h)
-    y3 = canvas.create_line(SP_w+column*3, SP_h, SP_w +column*3, height-SP_h)
-    y4 = canvas.create_line(SP_w+column*4, SP_h, SP_w +column*4, height-SP_h)
-    y5 = canvas.create_line(SP_w+column*5, SP_h, SP_w +column*5, height-SP_h)
-    y6 = canvas.create_line(SP_w+column*6, SP_h, SP_w +column*6, height-SP_h)
+    for i in columns:
+        i = canvas.create_line(SP_w+column*i, SP_h, SP_w +column*i, height-SP_h)
     a=canvas.create_rectangle(1,1,2,2)
     showposition()
     canvas.pack()
