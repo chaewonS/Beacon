34133c15b66c859fde09db6cf083d7897219cdeb
alalai012 <65814554+alalai012@users.noreply.github.com>
2022-04-06 20:56:25 +0900

    [선주] sort

--- twobeacon.py
+++ twobeacon.py
@@ -91,7 +91,7 @@ if __name__ == '__main__':
 
     while True:
         top2_list = [Beacon('00:19:01:70:81:ed','-100'),Beacon('00:19:01:70:82:62','-100')]
-
+        
         maclist = []
         returnedList = blescan.parse_events(sock, 10)
         #print("----------")
@@ -103,20 +103,19 @@ if __name__ == '__main__':
                 
                 now_mac = beacon[:17]
                 now_rssi = beacon[66:] 
-
+                
                 maclist =[]
-                top2_list.sort(key= lambda x : x.getRSSI())
+                top2_list.sort(key= lambda x : x.RSSI)
                 for x in top2_list:
                     maclist.append(x.getMAC())
 
                 if (now_mac in maclist) and (top2_list[maclist.index(now_mac)].getRSSI() < now_rssi):#1. 리스트에 이미 있는 비콘이며, 더 가까워진 경우.
                     if len(top2_list) <2:#비콘이 하나 있을 때
                         top2_list[maclist.index(now_mac)].RSSI = now_rssi
-                        #top2_list.sort(key= lambda x : x.getRSSI())
                     else:
                     #2개가 꽉 찼음을 발견했을 때
                         top2_list[maclist.index(now_mac)].RSSI = now_rssi #동일한 비콘의 RSSI를 수정한다.
-                        top2_list.sort(key= lambda x : x.getRSSI())
+                        #top2_list.sort(key= lambda x : x.RSSI)
                         #if top2_list[0].G == top2_list[1].G:
                         print("==============================")
                         for x in top2_list:
@@ -128,13 +127,11 @@ if __name__ == '__main__':
                     #이하 코드 같음
                     if len(top2_list) <2 and top2_list[0].G == info[now_mac]["group"]:
                         top2_list.append(Beacon(now_mac, now_rssi)) #리스트에 추가
-                        top2_list.sort(key= lambda x : x.getRSSI())
                     
                     else:
                         #top2_list[0].G == info[now_mac]["group"]: #큐 동작
                         top2_list[0] = Beacon(now_mac, now_rssi)  #rssi가 가장 작은 비콘을 삭제하고 추가
                         top2_list[1] = Beacon('00:19:01:70:81:ed','-100') #그룹 비콘만을 나타내기 위해 뒤의 자리를 비워줌
-                        top2_list.sort(key= lambda x : x.getRSSI())
                        # for x in top2_list:
                        #     if top2_list[0].RSSI == -100 or top2_list[1].RSSI == -100:
                        #         break
